72ed397b43347b6916e3a48c63e6702dfb17dcde
stefandeissler <214100949+stefandeissler@users.noreply.github.com>
2025-09-21 20:56:20 +0100

    Complete function getComputerChoice
    Add the following steps to the function code:
        - Selection of a random number (0, 1, or 2)
        - Converting the random number into a string
            (0 = "rock", 1 = "paper", 2 = "scissors")
        - Returning the string

--- javascript.js
+++ javascript.js
@@ -1,5 +1,21 @@
 function getComputerChoice() {
-// Declare local function variables
+    // Declare local function variables
     let computerChoice = "";
     let randomNumber = 0;
+
+    // Randomly choose 0, 1, or 2 as random number
+    randomNumber = Math.floor(Math.random() * 3);
+
+    // Convert random number into computer's choice
+    switch (randomNumber) {
+        case 0:
+            computerChoice = "rock";
+        case 1:
+            computerChoice = "paper";
+        case 2:
+            computerChoice = "scissors";
+    }
+
+    // Return result of selection process
+    return computerChoice;
 }
